04cd025c43bc92407e57fafd88cb2465c89e5e65
ryutaromori <ryutaro.mori.personal@gmail.com>
2020-04-01 20:51:27 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 
     get '/omnisearch', to: 'search#omnisearch'
   end
-
+  
+ get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  !request.xhr? && request.format.html?
+ end
 
 end
