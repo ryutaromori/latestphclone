6d26ef7322b5c5dda5247c5c4e381b6048ab952d
ryutaromori <ryutaro.mori.personal@gmail.com>
2020-04-01 17:27:37 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,6 @@ var webpack = require("webpack");
 var plugins = [];  //plugins for both dev and production
 var devPlugins = [];  //plugins for dev
 
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
 var prodPlugins = [
   new webpack.DefinePlugin({
     'process.env': {
