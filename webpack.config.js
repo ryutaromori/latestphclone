69dbc823ba728c9cf07c8732e50c68c8ed021e88
ryutaromori <ryutaro.mori.personal@gmail.com>
2020-04-01 17:44:49 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ["@babel/preset-env", 'react']
+          presets: ["@babel/preset-env", '@babel/react']
         }
       }
     ]
